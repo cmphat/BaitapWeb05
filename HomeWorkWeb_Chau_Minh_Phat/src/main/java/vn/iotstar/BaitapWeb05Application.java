860fd7e7a3c234d1fc144ee3df449a5be45c8dc6
photoconnect <photoconnect@example.com>
2026-09-12 23:59:58 +0700

    Bài 5: Cấu hình Spring Boot 4 với JSP và JSTL

---
+++ HomeWorkWeb_Chau_Minh_Phat/src/main/java/vn/iotstar/BaitapWeb05Application.java
@@ -0,0 +1,19 @@
+package vn.iotstar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class BaitapWeb05Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(BaitapWeb05Application.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaitapWeb05Application.class, args);
+    }
+}
